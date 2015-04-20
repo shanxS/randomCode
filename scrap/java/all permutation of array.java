5ee8accbf680e282e3aed8e2af3d7957da854555
shanxS <shanx.shashank@gmail.com>
2015-04-20 22:23:08 +0530

    all permutations of array

---
+++ scrap/java/all permutation of array.java
@@ -0,0 +1,57 @@
+// r2, q2, set 5, amazon
+// Write all possible permutations of a array of size z.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    private static List<Integer[]> arrays = new ArrayList<>();
+
+    public static void main(String[] args)
+    {
+        Integer[] array = {1,2,3,4};
+        List<Integer> partialArray = new ArrayList<>();
+        List<Integer> bag = new ArrayList<>(Arrays.asList(array));
+        permute(bag, partialArray, 3);
+
+        for (Integer[] a : arrays)
+        {
+            for (Integer value : a)
+            {
+                System.out.print(value + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    private static void permute(List<Integer> bag, List<Integer> partialArray, Integer size)
+    {
+        if (partialArray.size() == size-1)
+        {
+            for (Integer value : bag)
+            {
+                List<Integer> newPartialArray = new ArrayList<>(partialArray);
+                newPartialArray.add(value);
+                arrays.add(newPartialArray.toArray(new Integer[newPartialArray.size()]));
+            }
+            return;
+        }
+
+        for (Integer value : bag)
+        {
+            List<Integer> newPartialArray = new ArrayList<>(partialArray);
+            newPartialArray.add(value);
+
+            List<Integer> newBag = new ArrayList<>(bag);
+            newBag.remove(value);
+
+            permute(newBag, newPartialArray, size);
+        }
+
+    }
+
+}
