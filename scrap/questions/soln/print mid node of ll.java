fdf29e49d101e78feb2716b2fe3f9519917a41ea
shanxS <shanx.shashank@gmail.com>
2015-06-15 20:24:17 +0530

    nth node from end in ll

---
+++ scrap/questions/soln/print mid node of ll.java
@@ -0,0 +1,122 @@
+// print mid node of ll
+
+public class Main
+{
+    public static void main(String[] er)
+    {
+        //Integer[] array = {1, 8, 3, 7, 4, 5};
+        Integer[] array = {1};
+        LL ll = new LL();
+        for (Integer value : array)
+        {
+            ll.insert(value);
+        }
+
+        ll.print();
+        System.out.println("------------------------");
+
+        System.out.print(getMiddle(ll.getHead()));
+
+    }
+
+    private static Integer getMiddle(Node node)
+    {
+        Node mid = node;
+        Integer count = 0;
+
+        while(node != null)
+        {
+            if (count > 1 && count%2 != 0)
+            {
+                mid = mid.getNext();
+            }
+
+            ++count;
+            node = node.getNext();
+        }
+
+        return mid.getValue();
+    }
+
+
+}
+
+class LL
+{
+    private Node head;
+
+    public LL()
+    {
+        this.head = null;
+    }
+
+    public void insert(Integer value)
+    {
+        if (head == null)
+        {
+            head = new Node(value);
+        }
+        else
+        {
+            Node cursor = head;
+            while(cursor.getNext() != null)
+            {
+                cursor = cursor.getNext();
+            }
+
+            cursor.setNext(new Node(value));
+        }
+    }
+
+    public void print()
+    {
+        LL.print(head);
+    }
+
+    public static void print(Node head)
+    {
+        Node cursor = head;
+
+        while(cursor != null)
+        {
+            System.out.print(cursor.getValue() + " ");
+            cursor = cursor.getNext();
+        }
+    }
+
+    public Node getHead()
+    {
+        return head;
+    }
+}
+
+class Node
+{
+    private Node next;
+    private Integer value;
+
+    public Node(Integer value)
+    {
+        this.value = value;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
+
+    public Integer getValue()
+    {
+        return value;
+    }
+
+    public void setValue(Integer value)
+    {
+        this.value = value;
+    }
+}
