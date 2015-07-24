0e253ccabdc46f878acd5bdedbb21ee6f86caf33
shanxS <shanx.shashank@gmail.com>
2015-07-24 23:53:50 +0530

    test

---
+++ scrap/java/junk/testcases.h
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      cout<<"4"<<endl; 
+      
+      cout<<"91 9911111 111"<<endl; // space separator
+      cout<<"91 99111111111"<<endl; // lenght
+      cout<<"89 9911111111"<<endl; // country code
+      cout<<"91 1111111111"<<endl; // operator code
+      cout<<"61 62sd065476"<<endl; // char
+      return 0;
+}
